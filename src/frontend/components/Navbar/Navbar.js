6e87fda0d2d9fe495b3221a7c7e3f09a1b260eae
Rahul Bakshi <rahulbakshi3@gmail.com>
2022-03-31 12:58:14 +0530

    feat: added homepage screen

---
+++ src/frontend/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import './navbar.css'
+export const Navbar = () => {
+
+    return (
+        <nav className="navbar ff-title">
+            <h2 className="text-2xl "><a>nerdli.</a></h2>
+            <ul className="navbar-right text-normal">
+                <li><a>Explore</a></li>
+                <li><a>Login</a></li>
+            </ul>
+        </nav>
+    )
+}
+
---
+++ src/frontend/pages/HomePage.js
@@ -0,0 +1,25 @@
+import "../styles/home.css"
+export const HomePage = () => {
+    return (
+        <main>
+            <section className="hero-section">
+                <div className="content-container">
+                    <h1 className="fw-semibold ff-title">Declutter Your Thoughts and Ideas </h1>
+                    <p>Learn to leverage the true potential of your ideas and thoughts to build a personal knowledge management system in this world of information overload. With nerdli take your skill of note taking to the next level.</p>
+                    <div className="btn-container">
+                        <button className="hero-btn"><a>Start Learning</a> </button>
+                        <img className="home-arrow" src="https://res.cloudinary.com/rahulb4/image/upload/v1648686353/scribbles-scribbles-73_uuxgql.svg" alt="" />
+                    </div>
+
+                </div>
+                <div className="hero-image-container">
+                    <img className="hero-img"
+                        src="https://res.cloudinary.com/rahulb4/image/upload/v1648683857/Skribblhead.png"
+                        alt="hero image" />
+                </div>
+            </section>
+        </main>
+
+
+    )
+}
