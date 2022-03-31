5d77897a6133218f6d2ce0623d39644830597d8b
Rahul Bakshi <rahulbakshi3@gmail.com>
2022-03-31 13:33:19 +0530

    fix: added proper alt tag description

--- src/frontend/pages/HomePage.js
+++ src/frontend/pages/HomePage.js
@@ -8,14 +8,14 @@ export const HomePage = () => {
                     <p>Learn to leverage the true potential of your ideas and thoughts to build a personal knowledge management system in this world of information overload. With nerdli take your skill of note taking to the next level.</p>
                     <div className="btn-container">
                         <button className="hero-btn"><a>Start Learning</a> </button>
-                        <img className="home-arrow" src="https://res.cloudinary.com/rahulb4/image/upload/v1648686353/scribbles-scribbles-73_uuxgql.svg" alt="" />
+                        <img className="home-arrow" src="https://res.cloudinary.com/rahulb4/image/upload/v1648686353/scribbles-scribbles-73_uuxgql.svg" alt="image showing arrow to button" />
                     </div>
 
                 </div>
                 <div className="hero-image-container">
                     <img className="hero-img"
                         src="https://res.cloudinary.com/rahulb4/image/upload/v1648683857/Skribblhead.png"
-                        alt="hero image" />
+                        alt="illustration showing a confused man" />
                 </div>
             </section>
         </main>
